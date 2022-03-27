f5cd27c0ca4bb1721b241804791aca1115380b4e
Brajesh Anokha <anokha777@gmail.com>
2022-03-27 22:20:27 +0530

    k8s configuration

---
+++ k8s-delete.sh
@@ -0,0 +1,4 @@
+kubectl delete -f microservice/auth.yaml
+kubectl delete -f microservice/client.yaml
+kubectl delete -f microservice/gateway.yaml
+kubectl delete -f microservice/user-mgt.yaml
---
+++ k8s-deploy.sh
@@ -0,0 +1,5 @@
+kubectl apply -f microservice/auth.yaml
+kubectl apply -f microservice/client.yaml
+kubectl apply -f microservice/gateway.yaml
+kubectl apply -f microservice/user-mgt.yaml
+
